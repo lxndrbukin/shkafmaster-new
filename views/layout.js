9a3fcd8a687bca61e4075f6e25b124104e24d206
Alexander Bukin <alex.bukin@hotmail.com>
2022-12-12 12:59:38 +0000

    Added a temporary favicon

--- views/layout.js
+++ views/layout.js
@@ -14,6 +14,7 @@ module.exports = ({ content, req }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="/styles/styles.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="icon" type="image/x-icon" href="/imgs/favicon.ico">
         <title>SHKAFMASTER - ${pageTitle({ req })}</title>
       </head>
       <body>
